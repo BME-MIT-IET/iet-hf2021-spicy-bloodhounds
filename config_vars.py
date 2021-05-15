35b81f9a47b8c3d2707c43fd80c1695eb022f6a9
AirinB <andreeamoraru090@gmail.com>
2021-05-15 17:22:31 +0200

    add docstrings

--- config_vars.py
+++ config_vars.py
@@ -2,6 +2,12 @@ import os
 
 
 class Config:
+    '''
+    In the Config the config variables are defined
+    DATABASE_URL will the the databse url in the format postgres://user:password@hostname/dbname
+    DISCORD_BOT_TOKEN is the bot token
+    BOT_PREFIX is the bot prefix
+    '''
     SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
     BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
     prefix = os.environ.get('BOT_PREFIX')
